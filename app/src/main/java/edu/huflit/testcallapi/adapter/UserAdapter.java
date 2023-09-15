6d5557e611b9d428b7f0e03a0cc1094836e0a339
AICY-GIT <143994513+AICY-GIT@users.noreply.github.com>
2023-09-15 10:39:29 +0700

    them den phan RecycleView

---
+++ app/src/main/java/edu/huflit/testcallapi/adapter/UserAdapter.java
@@ -0,0 +1,59 @@
+package edu.huflit.testcallapi.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.huflit.testcallapi.R;
+import edu.huflit.testcallapi.model.User;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder>{
+
+    private final List<User> mListUser;
+
+    public UserAdapter(List<User> mListUser) {
+        this.mListUser = mListUser;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user,parent,false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = mListUser.get(position);
+        if(user == null){
+            return;
+        }
+        holder.tvId.setText(String.valueOf(user.getId()));
+        holder.tvTitle.setText(String.valueOf(user.getTitle()));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mListUser !=null){
+            return  mListUser.size();
+        }
+        return 0;
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvId,tvTitle;
+
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvId = itemView.findViewById(R.id.tv_id);
+            tvTitle = itemView.findViewById(R.id.tv_titile);
+        }
+    }
+}
